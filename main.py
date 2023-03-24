d92d71681068bb14c9419d080d0c176cadc769ae
Tafadzwa03kusie <124145220+Tafadzwa03kusie@users.noreply.github.com>
2023-03-25 00:58:43 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
-import dash
-import dash_core_components as dcc
-import dash_html_components as html
-from dash.dependencies import Input, Output, State
+import streamlit as st
 import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import time
+import streamlit.components.v1 as components
+st.set_page_config(layout="wide", initial_sidebar_state="expanded", page_icon="@", page_title="Visualizing NSE")
+# set_page_config => https://github.com/streamlit/streamlit/issues/1770
 
-app = dash.Dash()
-app.title = "Stock Visualization"
-app.layout = html.Div(children=[
-    html.H1('Stock Visualization Dashboard'),
-    html.H4('Nairobi Securities Exchange'),
-    dcc.Input(id="input", value='', type='text'),
-    html.Div(id="output-graph")
-])
+# load the available data and overview
+path = "./data/data.csv"
+
+# sidebar
+st.sidebar.header("Nairobi Securities Exchange Stock Prices")
+data = st.sidebar.file_uploader("Upload Datase", type=["csv"])
 
 
+menu = ["Ticker", "Company", "Price", "Date"]
+selection = st.sidebar.selectbox("Key Performance Indicator", menu)
+if selection =="Ticker":
+    st.subheader("Display data")
+    st. dataframe(df.head())
